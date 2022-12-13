5442f5e6975f7c6a326c3f39c32120066aacc327
Virendra Arekar <virendra.arekar@gmail.com>
2022-12-13 19:56:03 +0530

    Profile Screen

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -8,10 +8,13 @@ import Copy from "../../assets/images/copy.png"
 import Loader from "../../components/Loader"
 import Gmail from "../../assets/images/email.png"
 import FB from "../../assets/images/fb.png"
+import EllipseImg from "../../assets/images/Ellipse.png"
+import {HiOutlinePencilAlt} from 'react-icons/hi'
 
 
 function Profile() {
     const copyText='olivaquiz.referral-50641'
+    const [tab , setTab] = useState(1)
      
      const copyToClipboard = () => {
         copy(copyText);
@@ -94,53 +97,136 @@ function Profile() {
         <Skeleton>
             <Loader loader={load} />
             
-            <div className='bg-[#152C4F] h-44 w-full rounded-br-full' >
-            <div className='flex justify-center text-3xl text-white'>
-                    <p className='align-center mt-16'>Invite your Friends</p>
-                </div>
+            <div className='bg-[#152C4F] h-56 w-full ' >
             </div>
-                <div className=' flex justify-center mt-5 relative bottom-16'>
-                    <div className=' bg-[#FCFCFC] h-[auto] w-5/6 rounded-xl sm:shadow-md shadow-inner'>
-                        <div className='flex justify-center '>
-                            <div className='text-2xl text-[#282828] font-medium mt-10'>
-                                Refer a friend! Give 500 Points, Get 500 Points
-                            </div>
-                        </div>
-                        <div className='flex justify-center '>
-                            <div className='text-sm text-[#282828] w-96 text-center mt-16'>
-                                Get your referral link to give your friends 500 pts when they join on Oliva Ministries Quizzes via your link/code. If they join it, we’ll reward you with 500 pts and more exciting rewards
-                            </div>
-                        </div>
-                        <div className='flex justify-center text-sm mt-4 '>
-                            <Link to={"/termsAndCondition"} className="underline">Terms & Conditions</Link>
-                        </div>
-                        <div className='flex justify-center text-sm mt-4 '>
-                            <div className=' text-sm font-bold'>
-                                Invite now using:
-                            </div>
-                        </div>
-                        <div className='flex justify-center text-sm mt-4 '>
-                            <img height={"30px"} width={"30px"} style={{ margin: "10px" }} src={Gmail} alt="email" />
-                            <img height={"30px"} width={"30px"} style={{ margin: "10px" }} src={FB} alt="email" />
-                        </div>
-                        <div className='flex justify-center text-sm mt-4 '>
-                            <p className='text-sm text-[#282828]'>Or copy your personal link</p>
-                        </div>
-                        <div className='flex justify-center text-sm mt-4 '>
-                            <div className='flex  justify-between border-solid border-2' style={{ height: "2rem", width: "20rem" }}>
-                                <p className='p-1'>{copyText}</p> 
-                               
-                                <span  style={{ height: "1.9rem", width: "2rem" ,float:"right",backgroundColor:"#001246" }}>
-                                <img className="p-2"src={Copy} onClick={copyToClipboard}></img>
-                                </span> 
-                            </div>
-                        </div>
+            <div className='flex justify-center  relative bottom-32'>
+                <div className=' bg-[#FCFCFC] h-[auto] w-3/4 rounded-xl sm:shadow-md shadow-inner'>
+                    <div class="grid grid-cols-7 gap-4 p-4">
+                        <div class="col-span-2 border-r-2 border-gray-500">
+                            <div className='relative w-36 h-36 mt-6 rounded-full border border-gray-500 mx-auto'>
+                                <img src={EllipseImg} alt="profile" className='w-36 h-36'/>
+                                <HiOutlinePencilAlt className="absolute right-0 text-2xl text-white bottom-6 p-1 bg-[#3D5890] border rounded-full" />
+                            </div> <br></br>
+                            <h3 className='text-center text-lg font-bold'>John Martin</h3>
+                            <div className='mr-6  ml-2 mt-4'>
+                                 <div className={`${tab === 1 ? 'bg-[#C1D0F0] text-[#3D5890] p-2' : 'p-2'} text-base font-bold cursor-pointer px-7 my-4`}  onClick={() => setTab(1)} >Profile Details</div>
 
+                                 <div className={`${tab === 2 ? 'bg-[#C1D0F0] text-[#3D5890] p-2' : 'p-2'} text-base font-bold cursor-pointer px-7 my-4`}  onClick={() => setTab(2)}>My Rewards</div>
+
+                                 <div className={`${tab === 3 ? 'bg-[#C1D0F0] text-[#3D5890] p-2' : 'p-2'} text-base font-bold cursor-pointer px-7 my-4`}  onClick={() => setTab(3)}>Quiz Payment details</div>
+
+                                 <div className={`${tab === 4 ? 'bg-[#C1D0F0] text-[#3D5890] p-2' : 'p-2'} text-base font-bold cursor-pointer px-7 my-4`}  onClick={() => setTab(4)}>Transaction</div>
+
+                                 <div className={`${tab === 5 ? 'bg-[#C1D0F0] text-[#3D5890] p-2' : 'p-2'} text-base font-bold cursor-pointer px-7 my-4`}  onClick={() => setTab(5)}>My Quizzes</div>
+
+                                 <div className={`${tab === 6 ? 'bg-[#C1D0F0] text-[#3D5890] p-2' : 'p-2'} text-base font-bold cursor-pointer px-7 my-4`}  onClick={() => setTab(6)}>Change Password</div>
+                            </div>
+                        </div>
+                        <div class="col-span-5 p-10">
+                            <h3 className='text-center text-2xl font-bold mb-12'>Profile Details</h3>
+                            <div className='mr-48 ml-16'>
+                                <div class="mb-8">
+                                    <label for="username" class="form-label inline-block mb-2 text-[#50565F] text-sm">
+                                        Username Or Name *
+                                    </label>
+                                    <input
+                                        type="text"
+                                        className="
+                                        form-control
+                                        block
+                                        w-full
+                                        px-3
+                                        py-1.5
+                                        font-normal
+                                        text-lg
+                                        text-gray-700
+                                        bg-white bg-clip-padding
+                                        border border-solid border-gray-300
+                                        rounded
+                                        transition
+                                        ease-in-out
+                                        m-0
+                                        focus:text-gray-900 focus:bg-white focus:border-[#BBBBBB] focus:outline-none
+                                        "
+                                        id="username"
+                                        placeholder="Username Or Name"
+                                    />
+                                </div>
+
+
+                                <div class="mb-8">
+                                    <label for="username" class="form-label inline-block mb-2 text-[#50565F] text-sm">
+                                        Email *
+                                    </label>
+                                    <input
+                                        type="text"
+                                        className="
+                                        form-control
+                                        block
+                                        w-full
+                                        px-3
+                                        py-1.5
+                                        text-lg
+                                        font-normal
+                                        text-gray-700
+                                        bg-white bg-clip-padding
+                                        border border-solid border-gray-300
+                                        rounded
+                                        transition
+                                        ease-in-out
+                                        m-0
+                                        focus:text-gray-900 focus:bg-white focus:border-[#BBBBBB] focus:outline-none
+                                        "
+                                        id="username"
+                                        placeholder="Enter email address"
+                                    />
+                                </div>
+
+
+                                <div class="mb-8">
+                                    <label for="username" class="form-label inline-block mb-2 text-[#50565F] text-sm">
+                                        My Address *
+                                    </label>
+                                    <textarea
+                                        type="text"
+                                        className="
+                                        form-control
+                                        block
+                                        w-full
+                                        px-3
+                                        py-1.5
+                                        text-lg
+                                        font-normal
+                                        text-gray-700
+                                        bg-white bg-clip-padding
+                                        border border-solid border-gray-300
+                                        rounded
+                                        transition
+                                        ease-in-out
+                                        m-0
+                                        focus:text-gray-900 focus:bg-white focus:border-[#BBBBBB] focus:outline-none
+                                        "
+                                        id="username"
+                                        rows={4}
+                                        placeholder="Enter email address"
+                                    >
+                                    </textarea>
+                                </div>
+                            </div>
+                            <div class="grid grid-cols-2 gap-12 ml-16">
+                                <button class="bg-[#3D5890] hover:bg-[#3D5890] text-white font-bold py-2 px-4 rounded">
+                                    Save Changes
+                                </button>
+                                <button class="border border-[#3D5890] text-[#3D5890] font-bold py-2 px-4 rounded">
+                                    Cancel
+                                </button>
+                            </div>
+                            
+                        </div>
+                        
                     </div>
                 </div>
-
-            
-
+            </div>
         </Skeleton>
     )
 }
